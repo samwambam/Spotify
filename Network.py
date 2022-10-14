c44757c6684431dfdec3351e0f33b580d2c0a2e8
Sammy Tran <sammy.tran1@ucalgary.ca>
2022-10-14 14:36:42 -0600

    First commit

---
+++ Network.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+#%matplotlib inline
+
+df = pd.read_csv('./archive/data.csv')
